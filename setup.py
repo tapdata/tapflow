44ded9bd8f39ad55f28b88ef99dbbca5a078da9e
dreamcoin1998 <gaojunbin98@gmail.com>
2024-11-19 16:06:21 +0800

    fix: update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='tapflow',
-    version='0.2.13',
+    version='0.2.14',
     packages=find_packages(),
     install_requires=required,
     include_package_data=True,
