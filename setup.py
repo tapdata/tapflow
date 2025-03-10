6f47da9b401b057ecc8b5ef4ea5f16be01c1d42d
dreamcoin1998 <gaojunbin98@gmail.com>
2025-03-10 18:36:28 +0800

    chore: Rename package from 'tapflow' to 'tapsh'
    - Updated setup.py to change package name from 'tapflow' to 'tapsh'
    - Maintained current version number at 0.2.73

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='tapflow',
+    name='tapsh',
     version='0.2.73',
     packages=find_packages(),
     install_requires=required,
